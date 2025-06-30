b97043249e69aa16d5f7dbeb45d9b73c720dbb74
agustin <agustingarcia280998@gmail.com>
2025-06-30 19:20:14 -0300

    first commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,43 @@
+<script setup>
+import PetCard from './PetCard.vue';
+
+const pets = [
+  {
+    id: 1,
+    nombre: 'Tito',
+    edad: '2 años',
+    raza: 'Labrador',
+    imagen: 'imagen'
+  },
+  {
+    id: 2,
+    nombre: 'Nilo',
+    edad: '3 años',
+    raza: 'Callejero',
+    imagen: 'imagen'
+  }
+]
+</script>
+
+<template>
+  <div >
+    <h1>🐶 Mascotas en Adopción</h1>
+    
+    <div>
+      <PetCard
+        v-for="pet in pets"
+        :key="pet.id"
+        :nombre="pet.nombre"
+        :edad="pet.edad"
+        :raza="pet.raza"
+        :imagen="pet.imagen"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/PetCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps(["nombre", "edad", "raza", "imagen"]);
+</script>
+
+<template>
+  <div class="card">
+    <img :src="imagen" alt="Foto de mascota" />
+    <h2>{{ nombre }}</h2>
+    <p>{{ raza }}</p>
+    <p>{{ edad }}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 200px;
+  width: 300px;
+  padding: 1rem;
+  color: black;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  text-align: center;
+  background: #fff;
+}
+
+.card img {
+  width: 100%;
+  height: auto;
+  border-radius: 6px;
+}
+</style>
